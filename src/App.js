a91b9bde3b9dfe46b631e85cfcd301da876673e1
katerinamakri <kathrin023@gmail.com>
2018-09-22 00:23:40 +0300

    Fixed alternating colors functionality

--- src/App.js
+++ src/App.js
@@ -7,25 +7,31 @@ class App extends Component {
 
     this.state = {
       quote:'',
-      author:''
+      author:'',
+      backgroundColor:'#d35400',
+      color:'#d35400'
     }
 
     this.fetchQuote = this.fetchQuote.bind(this)
     this.getData = this.getData.bind(this)
     this.fetchQuote = this.fetchQuote.bind(this)
+    this.changeColor = this.changeColor.bind(this)
   }
 
 
   componentDidMount() {
-    // window.gm_authFailure = this.gm_authFailure;
     this.fetchQuote(this.getData);    
+  }
+
+  componentWillMount(){
+    document.body.style.backgroundColor = this.state.backgroundColor
   }
 
   fetchQuote (getData) {
 
     getData( `https://andruxnet-random-famous-quotes.p.mashape.com/` )
     .then((data) => {
-      console.log("fetched data", data);
+      // console.log("fetched data", data);
 
       this.setState({
         quote: data[0].quote,
@@ -51,21 +57,39 @@ class App extends Component {
     .then(response => response.json()); // parses response to JSON
   }
 
-  newQuote (fetchQuote, getData){
+  newQuote (fetchQuote, getData, changeColor){
     this.fetchQuote(getData);
+    this.changeColor();
+    // console.log ( this.state.backgroundColor)
+  }
+
+  changeColor (){
+    const colorArray = ['#db0a5b', '#d91e18', '#cf000f', '#c0392b', '#96281b', '#736598','#8c14fc','#a537fd', '#9b59b6',
+    '#913d88', '#674172', '#4d13d1', '#336e7b', '#013243', '#2c3e50', '#24252a', '#22a7f0',
+    '#2574a9', '#3a539b', '#1f3a93', '#5333ed', '#1e824c', '#2e3131']
+    let changeColorRandomly = Math.floor(Math.random() * 24)
+    
+    this.setState({
+      backgroundColor: colorArray[changeColorRandomly],
+    })
   }
 
   render() {
+
+    document.body.style.backgroundColor = this.state.backgroundColor
+
     return (
-      <div className="App">
+      <div className="App" >
         <div className="box" id="quote-box">
           <div>
-            <p id="text">{this.state.quote}</p>
-            <p id="author">{this.state.author}</p>
+            <p id="text" style={{color:this.state.backgroundColor}} ><i className="fas fa-quote-left"></i> {this.state.quote}</p>
+            <p id="author" style={{color:this.state.backgroundColor}} >- {this.state.author} -</p>
           </div>
           <div className="buttons-container">
-            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/intent/tweet" className="twitter" id="tweet-quote"> <i className="fab fa-twitter-square fa-2x"></i> </a>        
-            <button className="next-quote" id="new-quote" onClick={() => this.newQuote(this.fetchQuote, this.getData) }>New Quote</button>
+            <a  href="https://twitter.com/intent/tweet" className="twitter" id="tweet-quote"> 
+              <i className="fab fa-twitter-square fa-2x" style={{color:this.state.backgroundColor}}></i> 
+            </a>        
+            <button className="next-quote" id="new-quote" style={{backgroundColor:this.state.backgroundColor}} onClick={() => this.newQuote(this.fetchQuote, this.getData) }>New Quote</button>
           </div>
         </div>
       </div>
